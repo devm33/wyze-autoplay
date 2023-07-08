248f22fb4129d9495ed12869536d429a147ce1d2
Devraj Mehta <devm33@github.com>
2023-07-08 14:54:12 -0400

    Add content script

---
+++ scripts/content.js
@@ -0,0 +1,21 @@
+/** Clicks all "play" buttons on the page. */
+function playAll(root) {
+  root.querySelectorAll('button').forEach(button => {
+    const hash = new URL(button.querySelector('use')?.getAttribute('href'), window.location).hash;
+    if (hash === '#24-filled-play-usage') {
+      console.log('[wyze-autoplay] Auto-playing', button.parentElement); 
+      button.click();
+    }
+  });
+}
+
+/** Watch the page for new elements and play them. */
+new MutationObserver(mutations => {
+  mutations.forEach(mutation => {
+    mutation.addedNodes.forEach(node => {
+      if (node instanceof HTMLElement) {
+        playAll(node);
+      }
+    });
+  });
+}).observe(document.body, { childList: true, subtree: true });
