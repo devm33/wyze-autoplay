a2ae6952ebc29ee8c249e0be58453aaf19eb60ec
Devraj Mehta <devm33@github.com>
2023-09-01 21:17:30 -0400

    Only run autoplay on view page

--- scripts/content.js
+++ scripts/content.js
@@ -1,26 +1,31 @@
 /** Clicks all "play" buttons on the page. */
 function playAll(root) {
-  root.querySelectorAll('button').forEach(button => {
-    const hash = new URL(button.querySelector('use')?.getAttribute('href'), window.location).hash;
-    if (hash === '#24-filled-play-usage') {
-      console.log('[wyze-autoplay] Auto-playing', button.parentElement); 
+  root.querySelectorAll("button").forEach((button) => {
+    const hash = new URL(
+      button.querySelector("use")?.getAttribute("href"),
+      window.location
+    ).hash;
+    if (hash === "#24-filled-play-usage") {
+      console.log("[wyze-autoplay] Auto-playing", button.parentElement);
       button.click();
     }
   });
 }
 
 /** Watch the page for new elements and play them. */
-new MutationObserver(mutations => {
-  mutations.forEach(mutation => {
-    mutation.addedNodes.forEach(node => {
-      if (node instanceof HTMLElement) {
-        playAll(node);
-      }
+if (window.location.hostname === "view.wyze.com") {
+  new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      mutation.addedNodes.forEach((node) => {
+        if (node instanceof HTMLElement) {
+          playAll(node);
+        }
+      });
     });
-  });
-}).observe(document.body, { childList: true, subtree: true });
+  }).observe(document.body, { childList: true, subtree: true });
+}
 
 /** Redirect from error page to view page. */
-if (window.location.pathname === '/api/auth/error') {
-  window.location.pathname = '/';
+if (window.location.pathname === "/api/auth/error") {
+  window.location.pathname = "/";
 }
